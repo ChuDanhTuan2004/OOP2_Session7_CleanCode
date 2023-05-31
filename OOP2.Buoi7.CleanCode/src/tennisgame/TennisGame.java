2e7f97a9b9e5d639c893ce72a7c17a952041954d
ChuDanhTuan2004 <114391058+ChuDanhTuan2004@users.noreply.github.com>
2023-05-31 13:39:17 +0700

    Update TennisGame.java

--- OOP2.Buoi7.CleanCode/src/tennisgame/TennisGame.java
+++ OOP2.Buoi7.CleanCode/src/tennisgame/TennisGame.java
@@ -3,6 +3,10 @@ package tennisgame;
 public class TennisGame {
 
     public static final String[] SCORE_NAMES = {"Love", "Fifteen", "Thirty", "Forty"};
+    public static final char LOVE = 0;
+    public static final char FIFTEEN = 1;
+    public static final char THIRTY = 2;
+    public static final char FORTY = 3;
     public static final int SCORE_CHECK_MATE = 4;
 
     public static void main(String[] args) {
@@ -16,7 +20,7 @@ public class TennisGame {
     public static String getResult(String player1, String player2, int score1, int score2) {
         String result = "";
         if (score1 == score2) {
-            if (score1 > SCORE_CHECK_MATE) {
+            if (score1 > FORTY) {
                 result = "Deuce";
             } else {
                 result = SCORE_NAMES[score1] + "-All";
